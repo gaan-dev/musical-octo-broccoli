f9f609e939456e69a25d6274e6959d0df3de4010
Sean Jewell <sean@gaan.dev>
2020-11-13 17:35:44 +0000

    add tailwind, add axios. Some basic layout done

---
+++ src/components/CardGame.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="h-screen flex items-center justify-center ">
+    Card game
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CardGame',
+  mounted(){
+    
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
