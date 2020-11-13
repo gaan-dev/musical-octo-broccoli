f82b2a06cf6eec3ca4ea3e7ea3ce98d21bb36495
Sean Jewell <sean@gaan.dev>
2020-11-13 18:29:41 +0000

    most of the logic out of the way. Added a cycling function as I realise you don't want to shuffle the deck after you've guessed, you want to shift it to the end

--- src/components/CardGame.vue
+++ src/components/CardGame.vue
@@ -1,15 +1,26 @@
 <template>
   <div class="h-screen flex flex-col items-center justify-center ">
-    <div>
+    <div class="flex">
+      {{ this.lives }} Lives | Score: {{ this.score }}
+    </div>
+    <div class="rounded border border-solid border-black shadow-lg my-4 p-4 relative w-64 h-128 flex items-center justify-center">
+      <div class="absolute left-0 top-0 p-2">
+        {{ topCard.suit }}
+      </div>
+      <div class="absolute right-0 bottom-0 p-2">
+        {{ topCard.suit }}
+      </div>
       {{ topCard.value }}
-      {{ topCard.suit }}
+    </div>
+    <div>
+      Will the next card be higher or lower?
     </div>
     <div class="flex">
-      <button class="bg-blue-200 rounded px-4 py-2" @click.prevent="higher">Higher</button>
-      <button class="bg-red-200 rounded px-4 py-2" @click.prevent="lower">Lower</button>
+      <button class="bg-blue-200 rounded px-4 py-2 mr-1" @click.prevent="higher">Higher</button>
+      <button class="bg-red-200 rounded px-4 py-2 ml-1" @click.prevent="lower">Lower</button>
     </div>
-    <div>
-      <button class="bg-orange-200 rounded px-4 py-2" @click.prevent="shuffle">Shuffle Deck</button>
+    <div class="my-2">
+      <button class="bg-orange-200 rounded px-4 py-1 text-sm" @click.prevent="reset">Shuffle Deck (Reset)</button>
     </div>
   </div>
 </template>
@@ -24,44 +35,76 @@
 
     data(){
       return {
-        cards: []
+        cards: [],
+        lives: 3,
+        score: 0
       }
     },
 
     computed:{
+
       topCard(){
         if(this.cards.length > 0){
           return this.cards[0];
         }
         return {suit: '', value: ''};
       },
+
       nextCard(){
         if(this.cards.length > 0){
           return this.cards[1];
         }
         return {suit: '', value: ''};
       }
+
     },
 
     methods:{
-      value(card){
-        let map = {'A': 1, 'J':11, 'Q': 12, 'K': 13};
-        if(Number.isFinite(card.value)){
-          return card.value;
+      convertValue(card){
+        let valueMap = {'A': 1, 'J':11, 'Q': 12, 'K': 13};
+        if(!isNaN(parseInt(card.value))){
+          return parseInt(card.value);
         }
-        return map[card.value];
+        return valueMap[card.value];
       },
 
       higher(){
+        let a = this.convertValue(this.topCard);
+        let b = this.convertValue(this.nextCard);
+
+        if(a >= b){
+          this.lives--;
+        }else{
+          this.score++;
+        }
+
+        this.cycleTopToBot();
       },
 
       lower(){
+        let a = this.convertValue(this.topCard);
+        let b = this.convertValue(this.nextCard);
+
+        if(a <= b){
+          this.lives--;
+        }else{
+          this.score++;
+        }
+        
+        this.cycleTopToBot();
 
       },
 
-      shuffle(){
+      cycleTopToBot(){
+        let c = this.cards.shift(this.cards);
+        this.cards.push(c);
+      },
+
+      reset(){
         this.cards = shuffle(this.cards);
-      }
+        this.score = 0;
+        this.lives = 3;
+      },
     },
 
     mounted(){
