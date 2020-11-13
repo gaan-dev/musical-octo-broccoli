7d9b1dfa932947f35631179cd92f6881966dd964
Sean Jewell <sean@gaan.dev>
2020-11-13 17:43:50 +0000

    Get cards from API. Shuffle cards

--- src/components/CardGame.vue
+++ src/components/CardGame.vue
@@ -5,12 +5,36 @@
 </template>
 
 <script>
-export default {
-  name: 'CardGame',
-  mounted(){
+  import axios from 'axios';
+
+  export default {
     
+    name: 'CardGame',
+
+    data(){
+      return {
+        cards: []
+      }
+    },
+
+    methods:{
+      //https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+      shuffleCards(cards){
+        for (let i = cards.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1));
+          [cards[i], cards[j]] = [cards[j], cards[i]];
+        }
+        return cards;
+      }
+    },
+
+    mounted(){
+      axios.get('https://higher-lower.code23.com/api/deck')
+        .then(response => {
+          this.cards = this.shuffleCards(response.data);
+        });
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
